03e2436078cf37168cf213f3d8326efd924ab335
Andry <a.kuprin@eagle.ru>
2024-08-09 20:57:25 +0300

    Added new tasks

--- Chip8/Computer.h
+++ Chip8/Computer.h
@@ -3,64 +3,57 @@
 #include <cstdint>
 #include <fstream>
 
+// TODO: перенести в .cpp
+
 class Computer
 {
 public:
-	// TODO:
-	// скопировать содержимое дампа в память компьютера
-	// задать начальное значение счетчика команд
-	Computer(const std::string& dump_path)
+
+	Computer(const std::string& dump_path) : m_program_counter(512)
 	{
 		std::ifstream file(dump_path, std::ios::binary);
 
 		if (file.is_open())
 		{
-			int i = 0;
+			int i = 512;
 
 			while (!file.eof())
 			{
-				file >> m_memory[i]; i++;
+				file >> m_memory[i];
+				i++;
+
+				if (i >= 4096)
+				{
+					break;
+				}
 			}
+
+			file.close();
 		}
-		file.close();
-		/*
-		от 0 до 511 включительно - бесполезная область памяти,
-		но с 80 до 160 лежат 16 символов в виде битовых масок
-		от 512 до 4095 включительно - место под наши программы
-		*/
-		m_program_counter = 512;
 	}
 
-	// TODO:
-	// прочитать команду (2 байта) 
-	// увеличить счетчик на 2
-	// выполнить команду
 	void step()
 	{
-		//int command = m_memory[m_program_counter];
+		const uint16_t* command = reinterpret_cast<uint16_t*>(&m_memory[m_program_counter]);
 
 		m_program_counter += 2;
 
-		//switch (command)
-		//{
-		//case 0x00E0:
-		//	blankScreen();
-		//	break;
+		switch (*command)
+		{
+		case 0x00E0:
+			blankScreen();
+			break;
 
-		//case 0x00EE:
-		//	returnFromFunction();
-		//	break;
+		case 0x00EE:
+			returnFromFunction();
+			break;
 
-		//default:
-		//	break;
-		//}
+		default:
+			break;
+		}
 	}
 
 private:
-	// TODO:
-	// 0x00E0 - очистить экран
-	// 0x00EE - возврат из функции
-
 	void blankScreen()
 	{
 		for (int i = 0; i < 1984; i++)
@@ -72,7 +65,30 @@ private:
 	void returnFromFunction()
 	{
 		m_program_counter = m_stack[m_stack_pointer - 1];
+		--m_stack_pointer;
 	}
+
+	// TODO: реализовать команды
+	// 0x1nnn - перескочить по адресу nnn
+	// 0x2nnn - перейти в подпрограмму по адресу nnn
+	// 0x3xkk - пропустить следующую команду, если регистр с номером x равен kk
+
+	// Справка: как понять, что в старшем разряде шестнадцатеричной записи стоит 1, 2 или 3?
+	// для двоичной системы счисления количество уникальных чисел, 
+	// которые можно закодировать с помощью x разрядов равно 2^x
+	
+	// для шестнадцатеричной системы счисления количество уникальных чисел,
+	// которые можно закодировать с помощью x разрядов равно 16^x
+
+	// пусть есть шестнадцатеричного число из 4 разрядов
+	// xyzw
+	// сколько уникальных чисел оно может закодировать? 65536 числел
+	// в двоичной системе счисления нам бы потребовалось 16 разрядов
+	// получается, что один разряд шестнадцатеричного числа в 4 раза "сильнее" одного разряда двоичного числа 
+	// тогда один разряд шестнадцатеричного числа соответствует 4 разрядам двоичного и можно
+	// сопоставить запись числа в шестнадцатеричной и двоичной записи следующим образом
+	//	x		y		z		w
+	//	0101	0101	0101	0101
 
 private:
 	uint16_t m_program_counter;
